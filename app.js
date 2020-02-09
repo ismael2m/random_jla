166daec31f25cac93bf8f6396bff8b224d8cf2cb
ismael2m <ismael.mmadi11@gmail.com>
2020-02-09 10:52:50 +0100

    fonction random character terminée

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-import jla from './datas/jla.js'
+import jla from './datas/jla.js';
+
 const app = {
     init : () => {
         app.start();
     },
 
     start : () => {
-        const playBtn = document.getElementsByClassName('app-button');
+        // Récupération des éléments du DOM
+        const appRoot = document.getElementsByClassName('app');
+        const heroName = document.createElement('h2');
+        const playBtn = document.createElement('button');
+        const heroImg =  document.getElementsByClassName('app-jla');
+        appRoot[0].appendChild(playBtn)
 
+        playBtn.innerHTML = 'Play';
+        playBtn.classList.add("app-button")
+        
+        //Fonction tirage au sort
         const random = () => {
-            const i = setInterval(() => console.log('test test'), 1000);
+            const i = setInterval(() => {
+                const index = Math.floor(Math.random() * Math.floor(jla.length))
+                heroImg[0].src = `assets/img/${jla[index].name}.jpg`;
+                heroName.innerHTML = jla[index].name;
+                heroName.classList.add('text-white')
+            }, 80);
 
             setTimeout(()=>{
                 clearInterval(i)
                 console.log('stop');
+                appRoot[0].appendChild(heroName)
+                playBtn.innerHTML = 'Rejouer';
             }, 3000);
+
+           
         }
 
-        playBtn[0].addEventListener('click', random)
+        playBtn.addEventListener('click', random)
     }
 }
 
