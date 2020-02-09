9646e8fa8a12521b7e9c95c27c21e08bcb93a54c
ismael2m <ismael.mmadi11@gmail.com>
2020-02-09 08:21:29 +0100

    mise en place js : système de choix aléatoires

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('ok')
+
+const app = {
+    init : () => {
+        app.start();
+    },
+
+    start : () => {
+        const playBtn = document.getElementsByClassName('app-button');
+
+        const random = () => {
+            const i = setInterval(() => console.log('test test'), 1000);
+
+            setTimeout(()=>{
+                clearInterval(i)
+                console.log('stop');
+            }, 3000);
+        }
+
+        playBtn[0].addEventListener('click', random)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
