33b51ce206249b20723fa8ea7513cc0f8de8b869
ismael2m <ismael.mmadi11@gmail.com>
2020-02-10 14:53:09 +0100

    méthode de désactivation sur les boutons jouer/rejouer

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ const app = {
         // Ajout de classes et de textes aux boutons
         playBtn.classList.add('app-button-play','btn-1');
         replayBtn.classList.add('app-button-none','btn-2');
-        heroName.innerHTML = 'Héros';
         replayBtn.innerHTML = 'Rejouer';
         playBtn.innerHTML = 'Jouer'; 
     },
@@ -44,29 +43,59 @@ const app = {
     start : () => {
         //Fonction tirage au sort random();
         const random = () => {
+            // Voir ligne
+            app.disabledPlayBtn();
             
             //Méthode qui affiche une image aléatoire toutes les 80 millisecondes
-            const i = setInterval(() => {
+            const j = setInterval(() => {
 
                 //Génère un nombre aléatoire entre 1 et 8(jla.lenght)
                 const index = Math.floor(Math.random() * Math.floor(jla.length));
                 heroImgDom[0].src = `assets/img/${jla[index].name}.jpg`;
                 heroNameDom[0].innerHTML = jla[index].name;
-                heroNameDom[0].classList.add('text-white', 'font-weight-bold');
+                heroNameDom[0].classList.add('app-hero');
             }, 80);
 
             // On stoppe le setInterval (ligne 73)
-            app.stopRandom(i);
+            app.stopRandom(j);
         }
 
         // Déclenchement de la fonction random
         playBtnDom[0].addEventListener('click', random);
 
         //Effacement de la liste quand on clique sur "rejouer" et random(); est relancée
-        replayBtnDom[0].addEventListener('click', () => {
-            appRootDom[0].children[4].innerHTML ='';
+        replayBtnDom[0].addEventListener('click', (evt) => {
+            appRootDom[0].children[4].innerHTML ='';  
             random();
+
+            // Voir ligne
+            app.disabledReplayBtn();
+
         });
+    },
+
+    // Désactivation du bouton Jouer pendant le tirage au sort : évite les clics intempstifs qui feraient bugger le jeu
+    disabledPlayBtn : () => {
+        const i = setInterval(() => {
+            playBtnDom[0].disabled = true;
+        }, 1);
+
+        setTimeout(() => {
+            clearInterval(i);
+        }, 1000)
+    },
+
+    // Désactivation du bouton Rejouer pendant 5 secondes : évite les clics intempstifs qui feraient bugger le jeu
+    disabledReplayBtn : () => {
+        const k = setInterval(() => {
+            replayBtnDom[0].disabled = true;
+        }, 1);
+
+        setTimeout(() => {
+            clearInterval(k);
+            replayBtnDom[0].disabled = false;
+
+        }, 5500)
     },
 
     // Méthode qui stoppe le setInverval stocké dans const i (ligne 49) au bout de 3 secondes
@@ -98,7 +127,7 @@ const app = {
             jlaHero[0].powers.map((power)=>{
                 const powersItem = document.createElement('li');
                 powersListDom[0].classList.add('app-list')
-                powersItem.classList.add('text-white');
+                powersItem.classList.add('app-powers');
                 powersItem.innerHTML = power;
                 powersListDom[0].appendChild(powersItem);
             });
