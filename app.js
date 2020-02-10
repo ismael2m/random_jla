76bfb4d6b8a6d026f1b1f10d68b8911fc479ccbd
ismael2m <ismael.mmadi11@gmail.com>
2020-02-10 12:02:08 +0100

    factorisation du code

--- app.js
+++ app.js
@@ -1,90 +1,110 @@
+// Import des données à utiliser pour l'application 
 import jla from './datas/jla.js';
+
+// Récupération des éléments du DOM
+const appRootDom = document.getElementsByClassName('app');
+const heroNameDom = document.getElementsByTagName('h2');
+const powersListDom = document.getElementsByTagName('ul');
+const playBtnDom = document.getElementsByClassName('btn-1');
+const replayBtnDom = document.getElementsByClassName('btn-2');
+const heroImgDom =  document.getElementsByClassName('app-jla');
 
 const app = {
     // Fonction lancée au chargement du DOM
     init : () => {
         app.domElement();
         app.start();
-        
     },
 
     // Méthode qui créé les éléments du DOM qui seront utilisés
     domElement : () => {
-        console.log('DOM construit')
         const appRoot = document.getElementsByClassName('app');
         const heroName = document.createElement('h2');
         const powersList = document.createElement('ul');
         const playBtn = document.createElement('button');
         const replayBtn = document.createElement('button');
-        const heroImg =  document.getElementsByClassName('app-jla');
-        appRoot[0].appendChild(heroName)
-        appRoot[0].appendChild(powersList)
-        appRoot[0].appendChild(playBtn)
-        appRoot[0].appendChild(replayBtn)
-        playBtn.classList.add('app-button-play','btn-1')
-        replayBtn.classList.add('app-button-none','btn-2')
+
+        // Création d'un [] qui stocke les différents éléments à injecter dans la <div class="app">
+        // Itération avec map() : chaque élément est injecté dans la <div class="app">
+        const arrayElmt = [heroName, powersList, playBtn, replayBtn];
+        arrayElmt.map((elmt) => {
+            appRoot[0].appendChild(elmt);
+        });
+
+        // Ajout de classes et de textes aux boutons
+        playBtn.classList.add('app-button-play','btn-1');
+        replayBtn.classList.add('app-button-none','btn-2');
         heroName.innerHTML = 'Héros';
         replayBtn.innerHTML = 'Rejouer';
-        playBtn.innerHTML = 'Jouer';
-        
+        playBtn.innerHTML = 'Jouer'; 
     },
 
-    start : () => {
-       // Récupération des éléments du DOM
-        const appRootDom = document.getElementsByClassName('app');
-        const heroNameDom = document.getElementsByTagName('h2');
-        const powersListDom = document.getElementsByTagName('ul');
-        const playBtnDom = document.getElementsByClassName('btn-1');
-        const replayBtnDom = document.getElementsByClassName('btn-2');
-        const heroImgDom =  document.getElementsByClassName('app-jla');
 
-        //Fonction tirage au sort
+    // Méthode principal qui gère toute la logique
+    start : () => {
+        //Fonction tirage au sort random();
         const random = () => {
+            
+            //Méthode qui affiche une image aléatoire toutes les 80 millisecondes
             const i = setInterval(() => {
-                const index = Math.floor(Math.random() * Math.floor(jla.length))
+
+                //Génère un nombre aléatoire entre 1 et 8(jla.lenght)
+                const index = Math.floor(Math.random() * Math.floor(jla.length));
                 heroImgDom[0].src = `assets/img/${jla[index].name}.jpg`;
                 heroNameDom[0].innerHTML = jla[index].name;
-                heroNameDom[0].classList.add('text-white', 'font-weight-bold')
+                heroNameDom[0].classList.add('text-white', 'font-weight-bold');
             }, 80);
 
-            setTimeout(()=>{
-                clearInterval(i)
-                console.log('stop');
-
-                const jlaResult =  heroNameDom[0].innerHTML;
-
-                setTimeout(() => {
-                    const jlaHero = jla.filter((hero) => {
-                        return hero.name === jlaResult;
-                    })
-                    playBtnDom[0].classList.remove('app-button-play')
-                    replayBtnDom[0].classList.remove('app-button-none')
-                    playBtnDom[0].classList.add('app-button-none')
-                    replayBtnDom[0].classList.add('app-button-play')
-
-                    console.log(jlaHero);
-                   
-                    jlaHero[0].powers.map((power)=>{
-                        const powersItem = document.createElement('li');
-                        powersListDom[0].classList.add('app-list')
-                        powersItem.classList.add('text-white');
-                        powersItem.innerHTML = power;
-                        powersListDom[0].appendChild(powersItem)
-                      
-                    })
-                    ;
-                }, 1000);
-
-            }, 3000);       
+            // On stoppe le setInterval (ligne 73)
+            app.stopRandom(i);
         }
+
         // Déclenchement de la fonction random
-        playBtnDom[0].addEventListener('click', random)
+        playBtnDom[0].addEventListener('click', random);
+
+        //Effacement de la liste quand on clique sur "rejouer" et random(); est relancée
         replayBtnDom[0].addEventListener('click', () => {
             appRootDom[0].children[4].innerHTML ='';
             random();
-        })
+        });
     },
 
-}
+    // Méthode qui stoppe le setInverval stocké dans const i (ligne 49) au bout de 3 secondes
+    stopRandom : (i) => {
+        setTimeout(() => {
+            clearInterval(i)
+            // Je récupère le nom du héros dans le DOM et je m'en sers pour faire un filter dans app.powerResult();
+            const jlaResult =  heroNameDom[0].innerHTML;
+
+            // On affiche les pouvoirs
+            app.powerResults(jlaResult);
+        }, 3000);
+    },
+
+    // Méthode qui affichent les pouvoirs du bon perso au bout d'une seconde (ligne 80)
+    powerResults : (result) => {
+        setTimeout(() => {
+            const jlaHero = jla.filter((hero) => {
+                return hero.name === result;
+            })
+
+            // Remplacement du bounton "jouer" par "Rejouer"
+            playBtnDom[0].classList.remove('app-button-play');
+            replayBtnDom[0].classList.remove('app-button-none');
+            playBtnDom[0].classList.add('app-button-none');
+            replayBtnDom[0].classList.add('app-button-play');
+
+            //J'affishe la liste des pouvoirs
+            jlaHero[0].powers.map((power)=>{
+                const powersItem = document.createElement('li');
+                powersListDom[0].classList.add('app-list')
+                powersItem.classList.add('text-white');
+                powersItem.innerHTML = power;
+                powersListDom[0].appendChild(powersItem);
+            });
+
+        }, 1000);
+    }
+};
 
 document.addEventListener('DOMContentLoaded', app.init);
